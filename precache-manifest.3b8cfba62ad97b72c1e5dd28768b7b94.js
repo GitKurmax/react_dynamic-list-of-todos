224c901fd8da82db02194fb71353970528065db6
GitKurmalex <alexandrmail32@gmail.com>
2019-06-06 22:16:31 +0300

    Updates

---
+++ precache-manifest.3b8cfba62ad97b72c1e5dd28768b7b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137f87873560d067510d81636754c08a",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "73b91c60ed3de8787e2e",
+    "url": "/react_dynamic-list-of-todos/static/css/main.3c94e1af.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/react_dynamic-list-of-todos/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "73b91c60ed3de8787e2e",
+    "url": "/react_dynamic-list-of-todos/static/js/main.e8ef4f3c.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
